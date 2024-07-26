0fffb201086926203ebb9e04b71e2616cdace4e7
Shyam Sainarayanan Varadharajan <164883991+shemzegem200@users.noreply.github.com>
2024-07-26 16:58:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const cloudinary = require('./utils/cloudinary.js');
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(/*cors({credentials:true,origin: '*', })*/);
+app.use(cors(/*{credentials:true,origin: '*', }*/));
 app.use(CookieParser());
 
 
